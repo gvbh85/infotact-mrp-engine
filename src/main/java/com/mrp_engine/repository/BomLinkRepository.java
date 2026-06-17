89c93a032f4d60a85b3cc65c811c3592d6e21244
Cobrooo <23l31a46a2@gmail.com>
2026-06-17 13:13:59 +0530

    Added MySQL recursive CTE query as alternative BOM explosion implementation

--- src/main/java/com/mrp_engine/repository/BomLinkRepository.java
+++ src/main/java/com/mrp_engine/repository/BomLinkRepository.java
@@ -26,4 +26,30 @@ public interface BomLinkRepository extends JpaRepository<BomLink, Long> {
             @Param("parentId") Long parentId, 
             @Param("childId") Long childId);
 	
+
+    @Query(value = """
+    WITH RECURSIVE bom_explosion AS (
+        SELECT bl.parent_item_id, bl.child_item_id,
+               bl.quantity_required AS multiplier
+        FROM bom_link bl
+        WHERE bl.parent_item_id = :productId
+
+        UNION ALL
+
+        SELECT bl.parent_item_id, bl.child_item_id,
+               be.multiplier * bl.quantity_required
+        FROM bom_link bl
+        INNER JOIN bom_explosion be
+            ON bl.parent_item_id = be.child_item_id
+    )
+    SELECT i.id AS itemId, i.name AS itemName,
+           i.type AS itemType, i.unit_of_measure AS unitOfMeasure,
+           SUM(be.multiplier) * :targetQty AS requiredQuantity
+    FROM bom_explosion be
+    JOIN item i ON i.id = be.child_item_id
+    WHERE i.type = 'RAW_MATERIAL'
+    GROUP BY i.id, i.name, i.type, i.unit_of_measure
+    """, nativeQuery = true)
+    List<Object[]> explodeBomNative(@Param("productId") Long productId,
+                                  @Param("targetQty") Double targetQty);
 }
